dd42dd42e136e428e9735428be71bd56982cf8a2
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2017-11-15 09:25:19 -0500

    perfect!

--- Shapeslab/src/shapes/Triangle.java
+++ Shapeslab/src/shapes/Triangle.java
@@ -16,25 +16,25 @@ public class Triangle implements Shape {
 	 * 
 	 * @param radius
 	 */
-	public Triangle(int height, int baselength) {
+	public Triangle(int height, int baselength) {  // here i make the variables usable across multiple methods inside a class
 		this.height = height;
 		this.baselength = baselength;
 	}
 
 	@Override
-	public double calculateArea() {
+	public double calculateArea() {  // here i calculate the area
 		double triangleA = baselength*height;
 		return triangleA;
 	}
 
 	@Override
-	public double calculatePerimeter() {
+	public double calculatePerimeter() { //here i calculate the perimeter of the triangle by multiplying the base by 3 because this is an equalateral   triangle  
 		double triangleP = 3*baselength;
 		return triangleP;
 	}
 
 	@Override
-	public String toString() {
+	public String toString() { // here i return the height, base length, area, and perimeter in a formated 
 		return "triangle height: " + height + " triangle baselength" + baselength + "Area: " + this.calculateArea() + " Perimeter: "
 				+ this.calculatePerimeter();
 	}
